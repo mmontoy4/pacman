49b3210c1c1ffc12eef70793507050aa622fbb1a
mmontoy4 <mmontoya@almata.cat>
2020-12-14 19:34:10 +0100

    He fet una prova seguint el tutorial de la web p5js.

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -4,12 +4,15 @@ function preload() {
 }
 
 function setup() {
-  // put setup code here
   createCanvas(400, 400);
 }
 
 function draw() {
-  // put drawing code here
-   background(220);
-   background(100);
+
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }

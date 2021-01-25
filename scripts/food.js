00c79679f53d5656d7f354e45cbac7e877087110
mmontoy4 <mmontoya@almata.cat>
2021-01-25 18:45:15 +0100

    He afegit la barra de navegació de la pàgina del Pacman.

--- scripts/food.js
+++ scripts/food.js
@@ -7,4 +7,8 @@ class Food extends GameObject {
       image(foodImg, this.coordX, this.coordY); //Per mostrar-la, hem de posar el menjar al preload.
     }
 
+  showInstanceMode(p, img){
+          p.image(img, this.coordX, this.coordY); //Per mostrar-la, hem de posar el food al preload.
+        }
+
   }
